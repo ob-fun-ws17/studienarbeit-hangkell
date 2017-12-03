4958d9dc7a6195addaf49de6ac589bac3ff27762
Hageneder <fhagened@hm.edu>
2017-12-03 18:21:53 +0100

    refactor Word to use (Char, Bool)

--- src/Word.hs
+++ src/Word.hs
@@ -3,21 +3,42 @@ Module      : Hangman Word Module
 Description : Library to handle a hangman solution word.
 -}
 module Word
-    (tryChar, charPositions) where
+    (tryChar, solveChar) where
 
 import Data.Char(toLower)
 import Data.List
 
--- | Checks if a given string is in the word.
+-- | Solution of a Hangman game. Unguessd chars are marked with a False.
+type SolutionWord = [(Char, Bool)]
+
+{- | Creates a SolutionWord from a given String
+
+>>> createSolutionWord "abc"
+[('a', false), ('b', false), ('b', false)]
+-}
+createSolutionWord ::
+  String -- ^ String to play with
+  SolutionWord -- ^ Solution to create a game with
+createSolutionWord word = map flip word (\char -> (char, false))
+
+{- | Checks if a given char is in the word.
+
+>>> tryChar 'a' "Hangman"
+True
+-}
 tryChar ::
   Char -- ^ The char the player played
-  -> String -- ^ The word that is being played3
+  -> SolutionWord -- ^ The word that is being played3
   -> Bool -- ^ wether guessed char was right
-tryChar try solution = null $ charPositions try solution
+tryChar try solution = null $ elemIndices try (map fst solution)
 
--- | Returns positions of a char in the given word.
-charPositions ::
-  Char -- ^ Char to search for
-  -> String -- ^ Solution word to search in
-  -> [Int] -- ^ List of positions in solution.
-charPositions try solution = elemIndices (toLower try) (map toLower solution)
+{- | Applys a guess on the solution.
+
+>>> solveChar a [('a', false), ('b', true)]
+[('a', True), ('b', True)]
+-}
+solveChar ::
+  SolutionWord -- ^ Current game state
+  -> Char -- ^ Char that player tries to add
+  -> SolutionWord -- ^ New state of progress
+solveChar solution try = map (\(char, state) -> (char, state || toLower char == toLower try)) solution
