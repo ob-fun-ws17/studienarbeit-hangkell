fd1b89d324fa347d483549c808eadb61c6f11e52
Hageneder <fhagened@hm.edu>
2017-12-03 15:03:41 +0100

    add charPositions

--- src/Word.hs
+++ src/Word.hs
@@ -1,15 +1,23 @@
 {-|
 Module      : Hangman Word Module
-Description : Library to handle a word currently being played.
+Description : Library to handle a hangman solution word.
 -}
 module Word
-    () where
+    (tryChar, charPositions) where
 
 import Data.Char(toLower)
+import Data.List
 
 -- | Checks if a given string is in the word.
 tryChar ::
   Char -- ^ The char the player played
   -> String -- ^ The word that is being played3
   -> Bool -- ^ wether guessed char was right
-tryChar try word = toLower try `elem` map toLower word
+tryChar try word = null charPositions
+
+-- | Returns positions of a char in the given word.
+charPositions ::
+  Char -- ^ Char to search for
+  -> String -- ^ Solution word to search in
+  -> [Int] -- ^ List of positions in solution.
+charPositions try solution = elemIndices (toLower try) (map toLower solution)
