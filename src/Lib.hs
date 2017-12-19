347299e66c8834c326e9ab4b03cf668a63284fca
daflowada <fhagened@hm.edu>
2017-12-19 11:58:39 +0100

    implement basic API with no connection to storage

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,7 +3,7 @@
 {-# LANGUAGE TypeOperators   #-}
 {-|
 Module      : Library
-Description : Library to handle RESTful logic.
+Description : Library to handle __RESTful__ API
 Copyright   : >implying
 License     : >implying
 Maintainer  : Florian Hageneder
@@ -15,25 +15,64 @@ module Lib
     , app
     ) where
 
+import Game (Game (..), newGame)
+import Player
+import Data.Maybe
 import Data.Aeson
 import Data.Aeson.TH
 import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
 
-data User = User
-  { userId        :: Int
-  , userFirstName :: String
-  , userLastName  :: String
-  } deriving (Eq, Show)
+-- ############################################################################
+-- Messages
 
-$(deriveJSON defaultOptions ''User)
+data TurnMessage = TurnMessage
+  { playerId :: Int
+  , playerSecret :: String
+  , guess :: Char
+  }
+$(deriveJSON defaultOptions ''TurnMessage)
 
-type API = "users" :> Get '[JSON] [User]
-        :<|> "users2" :> Get '[JSON] [User]
+-- ############################################################################
+-- API
 
-startApp :: IO ()
-startApp = run 8080 app
+type API = "games" :> Get '[JSON] [Game] -- Get all games
+        :<|> "games" :> Post '[JSON] Game -- Create session
+        :<|> "games" :> Capture "gid" Int :> Get '[JSON] Game -- Get single game
+        :<|> "games" :> Capture "gid" Int :> ReqBody '[JSON] TurnMessage :> Put '[JSON] Game -- make a Turn
+        :<|> "games" :> Capture "gid" Int :> ReqBody '[JSON] TurnMessage :> Put '[JSON] Game -- solve Game
+        :<|> "games" :> Capture "gid" Int :> "players" :> Post '[JSON] Player -- create Player
+
+
+server :: Server API
+server = allGames
+    :<|> createGame
+    :<|> getGame
+    :<|> turnGame
+    :<|> solveGame
+    :<|> createPlayer
+
+    where allGames :: Handler [Game]
+          allGames = return [ ]
+
+          createGame :: Handler Game
+          createGame = return $ fromJust (newGame "Test")
+
+          getGame :: Int -> Handler Game
+          getGame gid = return $ fromJust (newGame "Test")
+
+          turnGame :: Int -> TurnMessage -> Handler Game
+          turnGame gid msg = return $ fromJust (newGame "Test")
+
+          solveGame :: Int -> TurnMessage -> Handler Game
+          solveGame gid msg = return $ fromJust (newGame "Test")
+
+          createPlayer :: Int -> Handler Player
+          createPlayer gid = return $ newPlayer 0
+
+-- ############################################################################
+-- Boilerplate
 
 app :: Application
 app = serve api server
@@ -41,11 +80,5 @@ app = serve api server
 api :: Proxy API
 api = Proxy
 
-server :: Server API
-server = return users
-      :<|> return [User 3 "Oliver" "Braun"]
-
-users :: [User]
-users = [ User 1 "Isaac" "Newton"
-        , User 2 "Albert" "Einstein"
-        ]
+startApp :: IO ()
+startApp = run 8080 app
