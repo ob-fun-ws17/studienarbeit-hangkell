6a8eb3ddf8574990f9499d9cbcdae6da8ca26690
daflowada <fhagened@hm.edu>
2017-12-19 22:28:38 +0100

    implement storage?

--- src/Storage.hs
+++ src/Storage.hs
@@ -1,5 +1,5 @@
-{-# LANGUAGE DataKinds       #-}
-{-# LANGUAGE TypeOperators   #-}
+-- {-# LANGUAGE DataKinds       #-}
+-- {-# LANGUAGE TypeOperators   #-}
 
 {-|
 Module      : Storage -> Persistance
@@ -22,44 +22,40 @@ import Data.List (find)
 gameFile :: FilePath
 gameFile = "storage.dat"
 
--- gameHandle = openFile gameFile ReadWriteMode
-
 -- ###########################################################################
 -- Read games
---
--- -- | Returns all the stored games.
--- loadGames :: IO [Game] -- ^ Loads all stored games from the storage
--- loadGames = do
---   content <- readFile gameFile
---   return $ read content
---
--- -- | Returns a single stored game or Nothing
--- loadGame ::
---   Int -- ^ GameID to get
---   -> Maybe Game
--- loadGame gid
---   | gid < 0 = Nothing
---   | otherwise = do
---       let games = loadGames :: IO [Game]
---       return find (\g -> gameId g == gid) games
---
--- -- ###########################################################################
--- -- Store games
---
--- -- | Overrides the whole storage with the given set of games
--- saveGames ::
---   [Game] -- ^ All games that should be persisted in the storage
---   -> IO () -- ^ writes everything the storage
--- saveGames games = do
---   writeFile gameFile (show games)
---
--- -- | Overrides the stored game with same ID or appends it to the list
--- saveGame ::
---   Game -- ^ Single game to update
---   -> IO () -- ^ Writes result to the file
--- saveGame update = do
---   games <- loadGames
---   let newGames = if null $ filter (\g -> gameId g == gameId update) games
---         then games ++ [update]
---         else map (\g -> if gameId g == gameId update then update else g) games
---       in saveGames newGames
+
+-- | Returns all the stored games.
+loadGames :: IO [Game] -- ^ Loads all stored games from the storage
+loadGames = do
+  content <- readFile gameFile
+  return $ read content
+
+-- | Returns a single stored game or Nothing
+loadGame ::
+  Int -- ^ GameID to get
+  -> IO (Maybe Game)
+loadGame gid = do
+  games <- loadGames
+  return $ find (\g -> gameId g == 0) games
+
+-- ###########################################################################
+-- Store games
+
+-- | Overrides the whole storage with the given set of games
+saveGames ::
+  [Game] -- ^ All games that should be persisted in the storage
+  -> IO () -- ^ writes everything the storage
+saveGames games = do
+  writeFile gameFile (show games)
+
+-- | Overrides the stored game with same ID or appends it to the list
+saveGame ::
+  Game -- ^ Single game to update
+  -> IO () -- ^ Writes result to the file
+saveGame update = do
+  games <- loadGames :: IO [Game]
+  let newGames = if null $ filter (\g -> gameId g == gameId update) games
+                    then games ++ [update]
+                    else map (\g -> if gameId g == gameId update then update else g) games
+      in saveGames newGames
