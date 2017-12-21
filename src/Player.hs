8929baa70976f94b3d908b2f8436d29b77d80211
DaFloWaDa <fhagened@hm.edu>
2017-12-21 15:31:12 +0100

    add way to access players in array by id and key

--- src/Player.hs
+++ src/Player.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE DataKinds       #-}
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeOperators   #-}
 {-# LANGUAGE OverloadedStrings #-}
 
@@ -16,16 +15,17 @@ module Player where
 
 import Data.Aeson
 import Data.Aeson.TH
+import Data.List (find)
 
 maxFaliures = 10
 
 -- | Data type to bundle Imformation about a player within an game session.
-data Player = Player {
-  playerId :: Int,
-  secret :: String,
-  failures :: Int,
-  isAlive :: Bool
-} deriving (Eq, Show, Read)
+data Player = Player
+  { playerId :: Int -- ^ ID of the player
+  , secret :: String -- ^ The secret key of the player
+  , failures :: Int -- ^ The amount of failures the player had
+  , isAlive :: Bool -- ^ If the player is still alive
+  } deriving (Eq, Show, Read)
 
 instance ToJSON Player where
   toJSON (Player pid secret failures isAlive) = object
@@ -60,3 +60,21 @@ wrongGuess p
   | not $ isAlive p = p
   | otherwise = let newFailures = failures p + 1
                 in Player (playerId p) (secret p) newFailures (newFailures < maxFaliures)
+
+-- | Grants access to a player with given id if the key does match
+getPlayerForId ::
+  [Player] -- ^ The set of players to search in
+  -> Int -- ^ The id of the player to fetch
+  -> String -- ^ The secret key of the player
+  -> Maybe Player -- ^ A player or Nothing if id or key did not match
+getPlayerForId [] _ _ = Nothing
+getPlayerForId _ _ "" = Nothing
+getPlayerForId xs pid key
+  | pid < 0 = Nothing
+  | otherwise = do
+      let player = find (\x -> playerId x == pid) xs
+      case player of
+        Nothing -> Nothing
+        Just p -> if secret p == key
+                    then return p
+                    else Nothing
--- test/PlayerSpec.hs
+++ test/PlayerSpec.hs
@@ -5,16 +5,16 @@ import Player
 import Test.Hspec
 
 -- S.U.T.
-p1 :: Player = Player 0 "" 0 True
-p1' :: Player = Player 0 "" 1 True
-p2 :: Player = Player 1 "" (maxFaliures - 1) True
-p2' :: Player = Player 1 "" maxFaliures False
+p1 :: Player = Player 0 "a" 0 True
+p1' :: Player = Player 0 "a" 1 True
+p2 :: Player = Player 1 "b" (maxFaliures - 1) True
+p2' :: Player = Player 1 "b" maxFaliures False
 
 spec :: Spec
 spec = do
   describe "new Player" $
     it "creates simple Player" $
-        newPlayer 0 `shouldBe` p1
+        playerId (newPlayer 0) `shouldBe` 0
   describe "playersAlive" $ do
     it "filters empty list" $
         playersAlive [] `shouldBe` []
@@ -31,3 +31,16 @@ spec = do
         wrongGuess p2 `shouldBe` p2'
     it "let the dead rest" $
         wrongGuess p2' `shouldBe` p2'
+  describe "getPlayerForID" $ do
+    it "ignores empty player sets" $
+        getPlayerForId [] 0 "a" `shouldBe` Nothing
+    it "ignores invalid player ids" $
+        getPlayerForId [p1] (-1) "a" `shouldBe` Nothing
+    it "ignores empty keys" $
+        getPlayerForId [p1] 0 "" `shouldBe` Nothing
+    it "rejects wrong keys" $
+        getPlayerForId [p1] 0 "wrong" `shouldBe` Nothing
+    it "returns player with right key" $
+        getPlayerForId [p1] 0 "a" `shouldBe` Just p1
+    it "returns nothing if there is no one to return" $
+        getPlayerForId [p1] 1 "b" `shouldBe` Nothing
