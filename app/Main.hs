9c3d6280ccd8c95b5b80f3e76312120cecd4ebae
DaFloWaDa <fhagened@hm.edu>
2017-12-21 18:02:06 +0100

    create storage if not present

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,16 @@
 module Main where
 
 import Api
+import Storage (gameFile)
+
+import Control.Monad (unless)
+import System.Directory (doesFileExist)
+import System.IO (writeFile)
 
 main :: IO ()
-main = startApp
+main = do
+  -- Initialize storage if it is not present
+  fileExist <- doesFileExist Storage.gameFile
+  unless fileExist $ writeFile Storage.gameFile "[]"
+  -- Start server
+  startApp
